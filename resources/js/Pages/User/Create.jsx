1fe9a6db865a7dcdc6201d37d59d5e3dab520afd
Alan Freire <alan.freire.87@gmail.com>
2023-02-09 13:19:41 -0300

    Fix: Selects form user

--- resources/js/Pages/User/Create.jsx
+++ resources/js/Pages/User/Create.jsx
@@ -15,7 +15,7 @@ function Create({ permissions }) {
         password_confirmation: "",
         status: "0",
         registry: "",
-        permission_id: "",
+        permission_id: "0",
     });
 
     const onHandleChange = (event) => {
@@ -44,6 +44,7 @@ function Create({ permissions }) {
                         <div className="mb-4">
                             <label htmlFor="permission_id" className="font-light">Permissão de acesso</label>
                             <Select value={data.permission_id} name={'permission_id'} handleChange={onHandleChange} required={true} placeholder="Digite a matricula">
+                                <option value={0} disabled>Selecione uma permissão</option>
                                 {permissions.map((item, index) => {
                                     return (
                                         <option value={item.id} key={index}>{item.description}</option>
@@ -79,13 +80,13 @@ function Create({ permissions }) {
                         </div>
                         <div className="flex items-center justify-center gap-4 mt-6">
                             <Button type={'submit'} processing={processing} color={'green'} className={"gap-2"}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="h-5 w-5" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
                                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
                                 </svg>
                                 <span>Enviar</span>
                             </Button>
                             <Button href={route('users.index')} className={'gap-2'}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="h-5 w-5" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
                                 </svg>
                                 <span>Voltar</span>
