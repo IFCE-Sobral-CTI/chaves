2dbd65c55e5beb58529562907d69fedaa918a3b5
Alan Freire <alan.freire.87@gmail.com>
2023-07-19 10:55:05 -0300

    Fix: vite configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        https: false,
+        host: true,
+        hmr: {host: 'localhost', protocol: 'ws'},
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
@@ -10,4 +15,9 @@ export default defineConfig({
         }),
         react(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js'
+        }
+    }
 });
